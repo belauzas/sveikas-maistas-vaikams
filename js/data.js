72ea9ba7523e7213bb1e3bd17101881271f50361
Rimantas <belauzas@yahoo.com>
2019-10-12 23:08:30 +0300

    List of recipes page design #4

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const recipes = [
+let recipes = [
     {
         name: 'Cukinijų tyrelė',
         img: ['cukiniju-tyrele.jpg'],
@@ -26,5 +26,45 @@ const recipes = [
         commets: [
             'Jei vaikas jau vyresnis ir matome, kad greitai išalksta, įdėkime papildomai šaukštelį kokybiško aliejaus, pavyzdžiui, ypač tyro (extra virgin) alyvuogių, šalto spaudimo kanapių ar linų sėmenų, ar tiesiog natūralaus sviesto.'
         ]
+    },
+    {
+        name: 'Pilno grūdo speltos keksiukai su razinomis',
+        img: ['pilno-grudo-speltos-keksiukai-su-razinomis.jpg'],
+        author: '',
+        age: {
+            fromNorm: { value: 12, param: 'm' }
+        },
+        ingredients: {
+            main: [
+                { name: 'sviestas', value: 150, param: 'g' },
+                [
+                    { name: 'agavų sirupas', value: 40, param: 'g' },       // multiple means - OR
+                    { name: 'nerafinuotas cukrus', value: 40, param: 'g' }
+                ],
+                [
+                    { name: 'avižų pienas', value: 100, param: 'ml' },
+                    { name: 'augalinis pienas', value: 100, param: 'ml' },
+                    { comment: 'Jei pienui nealergiškas, galite keisti ir natūraliu karvės pienu.' }
+                ],
+                { name: 'kiaušiniai', value: 2 },                           // no param -> 'units' ('vnt')
+                { name: 'razinos', value: 50, param: 'g' },
+                { name: 'pilno grūdo speltos miltai', value: 300, param: 'ml' },
+                { name: 'maltų meduolių prieskoniai', value: 1, param: 'ts',      // ts - tea spoon
+                    comment: 'Galite pasidaryti jų mišinį patys iš maltų gvazdikėlių, imbierų, žvaigždinio anyžiaus, kardamono bei cinamono' },
+                { name: 'kepimo milteliai', value: 2, param: 'ts' }
+            ]
+        },
+        process: [
+            'Sviestą ištirpiname puode ir nukeliame nuo viryklės',
+            'Į puodą įdedame agavų sirupą arba cukrų, supilame avižų pieną ir gerai išmaišome',
+            'Atskiriame kiaušinių baltymus nuo trynių, trynius suverčiame į bendrą masę, o baltymus išplakame iki putų',
+            'Puode esančius produktus sumaišome su išplaktais kiaušinio baltymais, supilame miltus, prieskonius, kepimo miltelius, gerai išmaišome, kol tešlos konsistencija taps panaši į grietinės',
+            'Jei tešla per skysta, įpilame daugiau pilno grūdo miltų',
+            'Keksiukų formeles pripildome gauta mase iki pusės, į kiekvieną formelę įdedame po 3-4 razinas',
+            'Kepame +175 C orkaitėje apie 25 minutes, kol keksiukai patamsės'
+        ]
     }
-]
+];
+
+
+recipes = [...recipes, ...recipes, ...recipes, ...recipes, ...recipes, ...recipes]
--- js/functions.js
+++ js/functions.js
@@ -1,2 +1,43 @@
 "use strict";
 
+const recipesList = (function(){
+
+const ageParams = {
+    m: 'mėnesiai'
+}
+
+const renderRepicesList = ( list ) => {
+    let HTML = '';
+    
+    for ( let i=0; i<list.length; i++ ) {
+        const r = list[i];
+        const { fromMin, fromNorm } = r.age;
+        const fm = fromMin;
+        const fn = fromNorm;
+        let ageHTML = '';
+        
+        if ( fromMin && fromNorm ) {
+            ageHTML = `<span title="${fm.value + ' ' + ageParams[fm.param]}">${fm.value + fm.param}</span>
+                        <span> - </span>
+                        <span title="${fn.value + ' ' + ageParams[fn.param]}">${fn.value + fn.param}</span>`;
+        }
+        if ( !fromMin && fromNorm ) {
+            ageHTML = `<span title="${fn.value + ' ' + ageParams[fn.param]}">${fn.value + fn.param}</span>`;
+        }
+
+        HTML += `<div class="recipe-card">
+                    <img src="./img/food/${r.img[0] ? r.img[0] : 'default.jpg'}"
+                        title="${r.name}">
+                    <div class="name">${r.name}</div>
+                    <div class="age">${ageHTML}</div>
+                </div>`;
+    }
+    
+    return document.querySelector('main > .recipes-list').innerHTML = HTML;
+}
+
+return {
+    render: renderRepicesList
+}
+
+})();
